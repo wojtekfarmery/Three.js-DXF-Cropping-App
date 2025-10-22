97f9e6bb3a1fcf91e2d3dbdceaffba53443e3480
wojtek35 <wojtek.kowcz35@gmail.com>
2025-10-22 16:36:55 -0400

    visible roi rectangle

--- src/pages/home/three-canvas/three-sixty-model.tsx
+++ src/pages/home/three-canvas/three-sixty-model.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, type FC } from "react";
 import * as THREE from "three";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, TransformControls } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
 import { type IDxf, type IPolylineEntity } from "dxf-parser";
 import { buildMeshFromPolyface } from "./build-mesh-from-polyface";
 
@@ -10,6 +11,10 @@ type ThreeSixtyModelProps = {
 
 export const ThreeSixtyModel: FC<ThreeSixtyModelProps> = ({ dxf }) => {
   const groupRef = useRef(new THREE.Group());
+  const roiMeshRef = useRef<THREE.Mesh>(null);
+  const transformRef = useRef<any>(null);
+  const orbitRef = useRef<any>(null);
+  const { camera } = useThree();
 
   useEffect(() => {
     if (!dxf || !groupRef.current) return;
@@ -18,33 +23,66 @@ export const ThreeSixtyModel: FC<ThreeSixtyModelProps> = ({ dxf }) => {
     group.clear();
 
     const meshGroup = new THREE.Group();
-
     for (const entity of dxf.entities) {
-      switch (entity.type) {
-        case "POLYLINE": {
-          const e = entity as IPolylineEntity;
-
-          const mesh = buildMeshFromPolyface(e);
-          if (mesh) {
-            meshGroup.add(mesh);
-          }
-          break;
-        }
+      if (entity.type === "POLYLINE") {
+        const mesh = buildMeshFromPolyface(entity as IPolylineEntity);
+        if (mesh) meshGroup.add(mesh);
       }
     }
 
     group.add(meshGroup);
 
-    console.log(meshGroup.children.length);
-  }, [dxf]);
+    const box = new THREE.Box3().setFromObject(meshGroup);
+    const size = new THREE.Vector3();
+    const center = new THREE.Vector3();
+    box.getSize(size);
+    box.getCenter(center);
+
+    const maxDim = Math.max(size.x, size.y, size.z);
+
+    camera.near = maxDim / 1000;
+    camera.far = maxDim * 10;
+    camera.updateProjectionMatrix();
+
+    const distance = maxDim * 1.5;
+    camera.position.set(center.x, center.y, distance);
+    camera.lookAt(center);
+
+    meshGroup.position.sub(center);
+
+    if (roiMeshRef.current) {
+      roiMeshRef.current.position.set(0, 0, 0);
+      roiMeshRef.current.scale.set(size.x * 0.3, size.y * 0.3, 1);
+    }
+
+    if (transformRef.current && roiMeshRef.current) {
+      transformRef.current.attach(roiMeshRef.current);
+    }
+
+    return () => transformRef.current?.detach();
+  }, [dxf, camera]);
 
   return (
     <>
       <ambientLight intensity={1.2} />
       <directionalLight position={[2, 2, 2]} intensity={1.2} />
       <axesHelper args={[500]} />
-      <OrbitControls enableZoom enablePan />
+
+      <OrbitControls ref={orbitRef} enableZoom enablePan makeDefault />
+      <TransformControls
+        ref={transformRef}
+        mode="translate"
+        showZ={false}
+        onMouseDown={() => (orbitRef.current.enabled = false)}
+        onMouseUp={() => (orbitRef.current.enabled = true)}
+      />
+
       <primitive object={groupRef.current} />
+
+      <mesh ref={roiMeshRef}>
+        <planeGeometry args={[1, 1]} />
+        <meshBasicMaterial color="orange" />
+      </mesh>
     </>
   );
 };
